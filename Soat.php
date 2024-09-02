1bb395bf26ded547cdae413b9aa8ab62e1927d31
vip2004I <uzbsupermen@gmail.com>
2024-09-02 21:47:25 +0500

    Add files via upload
    profil soat test

---
+++ Soat.php
@@ -0,0 +1,15 @@
+
+<?php
+
+if (!file_exists('madeline.php')) {
+    copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
+}
+include 'madeline.php';
+
+$MadelineProto = new \danog\MadelineProto\API('session.madeline');
+$MadelineProto->start();
+$time=date("H:i",strtotime("5 hour"));
+$MadelineProto->account->updateProfile(['first_name'=>"$time Coder",'about'=>"👨‍💻Bizning Aniq Vaqt:⌚️ $time "]);
+$Bool = $MadelineProto->account->updateStatus(['offline' => false ]);
+?>
+
